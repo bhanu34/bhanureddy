9bd2e9e84f4a2233d108c42a2ff97f2ac4794dd7
Bhanu Pratap Reddy <bhanu93@live.com>
2026-08-15 16:26:14 +0530

    Wrap app and pages with ThemeProvider and Layout

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,13 @@
 const React = require('react')
+const { ThemeProvider } = require('./src/context/ThemeContext')
 
-export function wrapPageElement({ element, props }) {
+// 1. Wrap entire app with ThemeContext (Global State & Color Controls)
+exports.wrapRootElement = ({ element }) => {
+  return <ThemeProvider>{element}</ThemeProvider>
+}
+
+// 2. Wrap individual pages with their designated Layout
+exports.wrapPageElement = ({ element, props }) => {
   const Layout = element.type.Layout ?? React.Fragment
 
   return <Layout {...props}>{element}</Layout>
